402bf6e2d3ee6407fd802a7cff99047d217c958a
xuzhen <xuzhen@zhangyue.com>
2020-09-15 10:18:00 +0800

    NEW:增加select

--- learn_go/learn_channel_test.go
+++ learn_go/learn_channel_test.go
@@ -2,6 +2,7 @@ package learn_go
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 )
 
@@ -51,4 +52,118 @@ func Test_Channel_Close(t *testing.T) {
 	}
 
 	fmt.Println("主协程结束")
+}
+
+//缓冲通道
+// 前面的channel通道都是非缓冲通道，每一次发送和接收都是阻塞式的。
+// 一个发送操作，对应一个接收操作，如果发送后未接收，就是阻塞的。
+// 同样对于接收者来说另一个发送之前它也是阻塞的。
+// 缓冲通道指的是有一个缓冲区，对于发送数据是将数据发送到缓冲区。当缓冲区满了之后才会被阻塞。
+func Test_Channel_Size(t *testing.T) {
+	//定义一个缓冲区大小为5的通道
+	ch1 := make(chan int, 5)
+	ch1 <- 1 //向缓冲区放入数据1 因为缓冲区的大小为5 放入一个1之后 还有四个空的缓冲区  所以还未阻塞
+	ch1 <- 2
+	ch1 <- 3
+	ch1 <- 4
+	ch1 <- 5 //此时缓冲区已经满 如果再加入 则会进入阻塞状态
+	//继续添加时会造成死锁 因为缓冲区满了 一直没有读取
+	ch1 <- 6 //fatal error: all goroutines are asleep - deadlock!
+	fmt.Println("main end")
+}
+
+// 对于缓冲区中的数据，是先进先出的原则。
+// 第一个进去的第一个被读取到，可以理解为，一个从管道的一端放入，一个从管道的另一端读取。
+
+//4、定向通道
+//而定向通道表示： 要么是只读通道，要么是只写通道。
+//chan <- T 只写通道
+//<- chan T 只读通道
+
+func Test_Channel_Direct(t *testing.T) {
+	ch1 := make(chan int)   //双向通道
+	ch2 := make(chan<- int) //只写通道
+	ch3 := make(<-chan int) //只读通道
+
+	//=========1================
+	//如果创建时候创建的就是双向通道
+	//则在子协程内部写入数据，读取的时候不受影响。
+	go WriteOnly(ch1)
+	data2 := <-ch1
+	fmt.Println("获取到只写通道中的数据是", data2)
+
+	//=========2================
+	//如果将定向通道ch2只写通道，作为参数传递。
+	//则不能读取到写回来的数据。
+	go WriteOnly(ch2)
+	//data := <-ch2 //不能读取会报错：invalid operation: <-ch2 (receive from send-only type chan<- int)
+
+	go ReadOnly(ch1) //这里可以传ch1 双向通道
+	ch1 <- 20        //向通道ch1中写入数据
+
+	//=========3================
+	go ReadOnly(ch3) //传递单向通道ch3 就无法向通道中写入数据
+
+	fmt.Println("结束")
+}
+
+//只读
+func ReadOnly(ch <-chan int) {
+	data := <-ch
+	fmt.Println("读取到通道的数据是：", data)
+}
+
+//只写
+func WriteOnly(ch chan<- int) {
+	//如果传进来的原本是双向通道
+	//但是函数本身接收的是一个只写的通道，则在此函数内部只允许写入数据不允许读取数据
+	//所以单向通道往往是作为参数传递
+	ch <- 10
+	fmt.Println("只写通道结束")
+}
+
+//5、死锁
+// 死锁是指两个或两个以上的协程的执行过程中，由于竞争资源而阻塞的现象，如果没有外力介入,则无法继续进行下去。
+// 死锁的出现的情况有很多种，但都大多数都是因为资源竞争和数据通信的时候引起的。
+
+//创建一个同步等待组的对象
+var wgp sync.WaitGroup
+
+func Test_Dead_Lock(t *testing.T) {
+	wgp.Add(4) //设置同步等待组的数量
+	go Sale1()
+	go Sale2()
+	go Sale3()
+	wgp.Wait() //主goroutine进入阻塞状态
+	fmt.Println("main end...")
+}
+
+func Sale1() {
+	fmt.Println("func1...")
+	wgp.Done() //执行完成 同步等待数量减1
+}
+func Sale2() {
+	defer wgp.Done()
+	fmt.Println("func2...")
+}
+func Sale3() {
+	defer wgp.Done() //使用延时执行来减去执行组的数量
+	fmt.Println("func3...")
+}
+
+//一个通道在一个主goroutine协程里同时进行读和写。也会造成死锁。
+func Test_Dead_Lock2(t *testing.T) {
+	c := make(chan int)
+	c <- 100 //向通道中写入数据
+	a := <-c //读取通道中的数据
+	fmt.Println(a)
+}
+
+//协程开启之前就放数据,还没有准备好，就放数据，就会造成死锁。
+func Test_Dead_Lock3(t *testing.T) {
+	c := make(chan int)
+	c <- 88
+	go func() {
+		<-c
+	}()
 }
---
+++ learn_go/learn_select_test.go
@@ -0,0 +1,42 @@
+package learn_go
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+// 我们在聊天过程中，有两条通道，一条专门负责发送消息给对方，另一条通道专门负责接收消息。
+// 虽然可以使用for循环来遍历每个通道的数据，达到同时接收到多个通道的数据，但是效率就比较差了。
+// 在Go语言中提供select关键字，可以同事响应多个通道的操作。
+
+func Test_Select(t *testing.T) {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go func() {
+		for {
+			select {
+			case <-ch1: // 如果ch1成功读到数据，则进行该case处理语句
+				fmt.Println("成功获取ch1的数据：", <-ch1)
+			case ch2 <- 1: //如果成功向ch2写入数据，则进行该case处理语句
+				fmt.Println("成功向通道ch2中写入数据")
+			case <-time.After(time.Second * 2):
+				//使用time.After 设置超时响应。如果迟迟接收不到以上的case就会响应超时。
+				fmt.Println("超时!!", time.After(time.Second*2))
+			}
+		}
+	}()
+
+	for i := 0; i < 10; i++ {
+		ch1 <- i
+		fmt.Println("ch1写入数据：", i)
+	}
+	for i := 0; i < 10; i++ {
+		str := <-ch2
+		fmt.Println("获取到ch2的数据：", str)
+	}
+
+	fmt.Println("时间记录：", time.After(time.Second*2))
+	// select 会一直等待等到某个 case 语句完成，
+	//也就是等到成功从 ch1 或者 ch2 中读到数据。则 select 语句结束。
+}
